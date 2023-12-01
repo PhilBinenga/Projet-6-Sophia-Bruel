19230b614fd96ac99c580ecf331b847d50b2f441
Philemon Binenga <philemonbinenga@gmail.com>
2023-12-01 19:05:18 +0100

    ajout des travaux

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,54 +1,69 @@
-// VARIABLE //
+const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
 
-const galleryElement = document.querySelector("#portfolio .gallery");
-const filterDom = document.querySelector(".filters");
-const token = sessionStorage.getItem("token");
 
-// Création d'une variable constante route de l'API
-const API = "http://localhost:5678/api";
-
-// WORK //
-async function fetchWorks() {
-    try {
-        const response = await fetch("http://localhost:5678/api/works");
-        return await response.json();
-    } catch (error) {
+// Récupération de l'API //
+const fetchWorks = () => {
+    fetch("http://localhost:5678/api/works")
+      .then((response) => response.json())
+      .then((data) => {
+        works = data;
+        createGallery(works);
+      })
+      .catch((error) => {
         console.log(error);
-    }
+      });
+  };
+  fetchWorks();
+
+  async function fetchCategories() {
+    const response = await fetch('http://localhost:5678/api/categories');
+    return await response.json();
+}
+fetchCategories();
+
+  // Apparition des travaux sur le DOM // 
+
+  function createGallery(objet) {
+    let galleries = "";
+    for (let work of objet) {
+        galleries += `
+        <figure>
+        <img src="${work.imageUrl}" alt="${work.title}">
+        <figcaption>${work.title}</figcaption>
+    </figure>`;  }
+    gallery.innerHTML = galleries;
+  }
+
+// Création des boutons par catégorie //
+
+function createAllButtons() {
+    fetchCategories().then((data) => {
+        console.log(data);
+        data.forEach((category) => {
+            createAllButtons(category);
+        });
+    });
 }
 
-// CATEGORIES // 
-async function fetchCategories() {
-    try {
-        const response = await fetch("http://localhost:5678/api/categories");
-        return await response.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
+const filterButton = document.createElement('button');
+categories.forEach(category => {
 
-const displayWorksGallery = (works) => {
-    const galleryElement = document.querySelector(".gallery"); 
-    galleryElement.innerHTML = "";
-    works.forEach(work => {
-        const figure = document.createElement("figure");
-        const image = document.createElement("img");
-        image.src = work.imageUrl;
-        const figCaption = document.createElement("figcaption");
+    //Création de bouttons pour chaques catégories
+    const filterButton = document.createElement('button');
 
-        figure.appendChild(image);
-        figure.appendChild(figCaption);
-        galleryElement.appendChild(figure);
-    })
-}
+    //Ajout du texte sur les boutons en fonction du nom de la catégorie
+    filterButton.textContent = category.name;
 
-const filterButtons = () => {
-    filters.innerHTLM = "
-    <button class="btn-tous">Tous</button>
-    <button class="btn-objets">Objets</button>
-    <button class="btn-appartements">Appartements</button>
-    <button class="btn-hotel">Hôtels & restaurants</button>
-    "
-};
+    //Ajout d'un ID sur chaque bouttons en fonction des catégories
+    filterButton.id = category.id;
 
-filtersButtons();
+    // "filterButton" enfant lié à son parent "filtersElement"
+    filtersElement.appendChild(filterButton);
+
+    //Ajout évenement au clic du boutton affichant des éléments en fonction de la catégorie sélectionnée.
+    filterButton.addEventListener('click', (event) => {
+        const filteredWorks = event.target.id === 'all' ? works : works.filter(work => work.categoryId == event.target.id);
+        displayWorks(filteredWorks);
+    });
+});
